c4d111726da688cb029e12a8f41dbb97ddf49957
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2022-01-06 12:34:20 +0530

    Integrated Firebase

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_id';
